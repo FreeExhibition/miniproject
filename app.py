821a9ee07a45a62048b0736b941b0713ac013c6b
kimjungjin <jungks9351@gmail.com>
2022-01-11 17:42:04 +0900

    feat : 리뷰 버튼 클릭시 리뷰 삭제

--- app.py
+++ app.py
@@ -12,9 +12,19 @@ conn = pymysql.connect(host='localhost', user='root',
 
 cursor = conn.cursor(pymysql.cursors.DictCursor);
 
-
 @app.route('/detail/<id>')
 def getDetail(id):
+
+    getIdSql = "select ex_id from exhibitions where ex_id =%s"
+
+    cursor.execute(getIdSql, id)
+
+    getId = cursor.fetchone();
+    print(getId)
+
+
+    # if not getId: return render_template("main.html")
+
     getExhibitionSql = "select * from exhibitions where ex_id = %s"
 
     cursor.execute(getExhibitionSql, id)
@@ -29,6 +39,17 @@ def getDetail(id):
     print(reviews)
 
     return render_template("detail.html", exhibition=exhibition, reviews=reviews)
+
+# 작성자가 들어왔을 때 지우는 API
+# 토큰이 있을 때만 가능하게 해야함
+@app.route('/reviews/<id>', methods=['DELETE'])
+def deleteReview(id):
+    id = request.form['id_give']
+    sql = "delete from reviews where id= %s"
+    cursor.execute(sql, id)
+    cursor.fetchone()
+    conn.commit()
+    return jsonify({'msg': '삭제 완료!'})
 
 
 if __name__ == '__main__':

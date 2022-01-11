c53bacab40cbfb27c5d6b54e4fde837e97eb9f59
jeongmin-dev <ljeongmin0908@gmail.com>
2022-01-11 09:12:41 +0900

    test updates

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)

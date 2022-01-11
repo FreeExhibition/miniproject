ecb6958b012675704d77683b1c154e4ba08b6e27
kimjungjin <jungks9351@gmail.com>
2022-01-11 17:35:21 +0900

    feat : Jinja2를 활용하여 mysql db 더미데이터를 이용한 GET 요청

--- app.py
+++ app.py
@@ -13,5 +13,23 @@ conn = pymysql.connect(host='localhost', user='root',
 cursor = conn.cursor(pymysql.cursors.DictCursor);
 
 
+@app.route('/detail/<id>')
+def getDetail(id):
+    getExhibitionSql = "select * from exhibitions where ex_id = %s"
+
+    cursor.execute(getExhibitionSql, id)
+
+    exhibition = cursor.fetchone()
+    print(exhibition)
+
+    getReviewSql = "select * from reviews where review_id =%s"
+    cursor.execute(getReviewSql, id)
+
+    reviews = cursor.fetchall()
+    print(reviews)
+
+    return render_template("detail.html", exhibition=exhibition, reviews=reviews)
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

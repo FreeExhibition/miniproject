ff36f92e61dc657be63cb286bb94b89d5e1e9c85
lshyun955 <lshyun955@gmail.com>
2022-01-13 15:44:03 +0900

    MainPage

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ conn = pymysql.connect(
     charset='utf8'
 )
 
+cursor = conn.cursor(pymysql.cursors.DictCursor);
+
 # JWT 토큰을 만들 때 필요한 비밀문자열
 # 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
 SECRET_KEY = 'freeExhib'
@@ -26,28 +28,14 @@ import datetime
 # 비밀번호를 암호화하여 DB에 저장
 import hashlib
 
+import random
+
 # HTML을 주는 부분
 @app.route('/')
 def home():
-    # 쿠키에서 토큰 받아올 때
-    tokenReceive = request.cookies.get('mytoken')
 
-    try:
-        payload = jwt.decode(tokenReceive, SECRET_KEY, algorithms=['HS256'])
 
-        # userId를 DB에서 찾는다.
-        with conn.cursor() as cursor:
-            sql = "SELECT * FROM users where user_id = %s"
-            cursor.execute(sql, (payload['userId']))
-            user = cursor.fetchone()
-
-        return render_template('index.html', userId=user[0], token=tokenReceive)
-
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login"))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login"))
-
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
@@ -57,6 +45,87 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+
+@app.route('/mypage')
+def mypage():
+    return render_template('mypage.html')
+
+
+@app.route('/exhibition', methods=['GET'])
+def exhibition():
+    with conn.cursor() as cursor:
+        cntSql = "SELECT count(*), exhibition_id FROM exhibitions GROUP BY exhibition_id"
+        cursor.execute(cntSql)
+
+        cnt = cursor.fetchall()
+        rowCount = len(cnt)
+        idList = []
+        randNumList = []
+        cardNum = 6
+        resultList = []
+
+        print(cnt)
+
+        for num in range(0, rowCount - 1):
+            idList.append(cnt[num][1])
+        # print(idList)
+
+
+        for i in range(0, cardNum):
+            randNum = random.randrange(0, rowCount - 1)
+            while idList[randNum] in randNumList:
+                randNum = random.randrange(0, rowCount - 1)
+            randNumList.append(idList[randNum])
+
+        for j in range(0, cardNum):
+            selectSql = "SELECT * FROM exhibitions WHERE exhibition_id = %s"
+            cursor.execute(selectSql, (randNumList[j]))
+            result = cursor.fetchall()
+            resultList.append(result)
+
+        print(resultList)
+
+    return jsonify({'results': resultList})
+
+
+@app.route('/likes', methods=['POST'])
+def like():
+    userId_receive = request.form["give_userId"]
+    exhibitionId_receive = request.form["give_exhibitionId"]
+    with conn.cursor() as cursor:
+        checkSql = "SELECT count(*) FROM users WHERE user_id = %s"
+        cursor.execute(checkSql, (userId_receive))
+        cnt = len(cursor.fetchall())
+        if cnt <= 0:
+            return jsonify({'msg': '찜목록 추가 실패!'})
+        else:
+            dupCheckSql = "SELECT count(*) FROM WishList WHERE exhibition_id2 = %s"
+            cursor.execute(dupCheckSql, int(exhibitionId_receive))
+            cnt2 = cursor.fetchall()
+
+            if cnt2[0][0] != 0:
+                deleteSql = "DELETE FROM WishList WHERE exhibition_id2 = %s"
+                cursor.execute(deleteSql, int(exhibitionId_receive))
+                conn.commit()
+                return jsonify({'msg': '찜목록에서 제거되었습니다!'})
+            else:
+                insertSql = "INSERT INTO WishList VALUES(%s, %s)"
+                result = cursor.execute(insertSql, (userId_receive, int(exhibitionId_receive)))
+                print(result)
+                conn.commit()
+                return jsonify({'msg': '찜목록에 추가되었습니다!'})
+
+@app.route('/user_like', methods=['GET'])
+def userLike():
+    userId_receive = request.args.get('userId')
+    print(userId_receive)
+    with conn.cursor() as cursor:
+        sql = "SELECT exhibition_id2 FROM WishList WHERE user_id2 = %s"
+        cursor.execute(sql, (userId_receive))
+        result = cursor.fetchall()
+        print(result)
+    return jsonify({'results': result})
 
 
 # 로그인, 회원가입을 위한 API
@@ -76,6 +145,7 @@ def apiRegister():
         conn.commit()
         return jsonify({'result': 'success'})
 
+
 @app.route('/users/checkDup', methods=['POST'])
 def checkDup():
     userReceive = request.form['userid_give']
@@ -87,6 +157,7 @@ def checkDup():
         exists = bool(user)
 
     return jsonify({'result': 'success', 'exists': exists})
+
 
 # [로그인 API]
 # userId, userPwd를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
@@ -126,88 +197,87 @@ def apiLogin():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-import random
-# HTML을 주는 부분
-@app.route('/exhibition', methods=['GET'])
-def exhibition():
+
+@app.route('/detail/<id>')
+def getDetail(id):
+    getIdSql = "select exhibition_id from exhibitions where exhibition_id =%s"
+
+    cursor.execute(getIdSql, id)
+
+    getId = cursor.fetchone();
+    print(getId)
+
+
+    if not getId: return render_template("index.html")
+
+    getExhibitionSql = "select * from exhibitions where exhibition_id = %s"
+
+    cursor.execute(getExhibitionSql, id)
+
+    exhibition = cursor.fetchone()
+    print(exhibition)
+
+    return render_template("detail.html", exhibition=exhibition)
+
+
+@app.route('/reviews/<id>', methods=['GET'])
+def getReviews(id):
+    getReviewSql = "select * from reviews where exhibition_id2 =%s"
+    cursor.execute(getReviewSql, id)
+
+    reviews = cursor.fetchall()
+    print(reviews)
+
+    return jsonify({'allReviews': reviews})
+
+
+@app.route('/reviews', methods=['POST'])
+def postReview():
+    try:
+        tokenReceive = request.cookies.get('mytoken')
+
+        payload = jwt.decode(tokenReceive, SECRET_KEY, algorithms=['HS256'])
+
+        with conn.cursor() as cursor:
+
+            getUsrIdSql = "select * from users where user_id = %s"
+
+            cursor.execute(getUsrIdSql, (payload['userId']))
+
+            user = cursor.fetchone()
+
+            print(user);
+
+            userId = user[0]
+            contentRecieve = request.form['content_give']
+            exIdRecieve = request.form['exhibitionId_give']
+
+            postSql = "insert into reviews ( user_id2,exhibition_id2, content) values ( %s, %s, %s)"
+
+            cursor.execute(postSql, (userId, exIdRecieve, contentRecieve))
+
+            conn.commit()
+            return jsonify({'msg': '리뷰 작성'})
+        return
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({'msg': '로그인 시간 만료'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'msg': '로그인 정보 없음'})
+
+
+# 작성자가 들어왔을 때 지우는 API
+# 토큰이 있을 때만 가능하게 해야함
+
+@app.route('/reviews/<id>', methods=['DELETE'])
+def deleteReview(id):
+    id = request.form['id_give']
     with conn.cursor() as cursor:
-        cntSql = "SELECT count(*), exhibition_id FROM exhibitions GROUP BY exhibition_id"
-        cursor.execute(cntSql)
-        today = datetime.date.today()
-        cnt = cursor.fetchall()
-        rowCount = len(cnt)
-        idList = []
-        randNumList = []
-        cardNum = 6
-        resultList = []
+        sql = "delete from reviews where exhibition_id2= %s"
+        cursor.execute(sql, id)
+        conn.commit()
 
-        for num in range(0, rowCount - 1):
-            idList.append(cnt[num][1])
-        # print(idList)
-
-        for i in range(0, cardNum):
-            randNum = random.randrange(0, rowCount-1)
-            while idList[randNum] in randNumList:
-                randNum = random.randrange(0, rowCount-1)
-            randNumList.append(idList[randNum])
-
-        for j in range(0, cardNum):
-            selectSql = "SELECT * FROM exhibitions WHERE exhibition_id = %s and end_date > %s"
-            cursor.execute(selectSql,(randNumList[j], today))
-            result = cursor.fetchall()
-            resultList.append(result)
-
-        print(resultList)
-    return jsonify({'results' : resultList})
-
-
-@app.route('/likes', methods=['POST'])
-def like():
-    userId_receive = request.form["give_userId"]
-    exhibitionId_receive = request.form["give_exhibitionId"]
-    with conn.cursor() as cursor:
-        checkSql = "SELECT count(*) FROM users WHERE user_id = %s"
-        cursor.execute(checkSql, (userId_receive))
-        cnt = len(cursor.fetchall())
-        if cnt <= 0:
-            return jsonify({'msg': '찜목록 추가 실패!'})
-        else:
-            dupCheckSql = "SELECT count(*) FROM wishList WHERE exhibition_id2 = %s AND user_id2 = %s"
-            cursor.execute(dupCheckSql, (int(exhibitionId_receive), userId_receive))
-            cnt2 = cursor.fetchall()
-
-            if cnt2[0][0] != 0:
-                deleteSql = "DELETE FROM wishList WHERE exhibition_id2 = %s AND user_id2 = %s"
-                cursor.execute(deleteSql, (int(exhibitionId_receive), userId_receive))
-                conn.commit()
-                return jsonify({'msg': '찜목록에서 제거되었습니다!'})
-            else:
-                insertSql = "INSERT INTO wishList VALUES(%s, %s)"
-                result = cursor.execute(insertSql, (userId_receive, int(exhibitionId_receive)))
-                print(result)
-                conn.commit()
-                return jsonify({'msg': '찜목록에 추가되었습니다!'})
-
-
-@app.route('/user_like', methods=['GET'])
-def userLike():
-    userId_receive = request.args.get('userId')
-    with conn.cursor() as cursor:
-        sql = "SELECT exhibition_id2 FROM wishList WHERE user_id2 = %s"
-        cursor.execute(sql, (userId_receive))
-        result = cursor.fetchall()
-    return jsonify({'results' : result})
-
-
-
-@app.route('/search', methods=['POST'])
-def search():
-    keyword_receive = request.form["give_keyword"]
-    with conn.cursor() as cursor:
-        sql = "SELECT exhibition_id FROM exhibitions WHERE title LIKE %s"
-        cursor.execute(sql,('%' + keyword_receive + '%'))
-        result = cursor.fetchall()
-    return jsonify({'results' : result})
+    return jsonify({'msg': '삭제 완료!'})
 
 
 if __name__ == '__main__':
